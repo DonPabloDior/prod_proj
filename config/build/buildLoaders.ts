ddefe8ef7c08af9b5e13abf7413fae4575ed87d3
Ermuhammed Mahanbetov <ermuhammed.makhanvetov@finnplay.com>
2024-07-23 06:59:29 +0600

    add config

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,25 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
+import webpack from "webpack"
+import { BuildOptions } from "./types/config"
+
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader'
+        ]
+    }
+
+    return [
+        {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+        },
+        cssLoader
+    ]
+}
